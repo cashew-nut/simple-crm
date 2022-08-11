626402d4e3245972bd51bcd17da9ad8bb71fc43d
Luke Cash <lukecash@live.com>
2022-08-11 22:28:39 +0100

    updated function

--- functions/record_selection.py
+++ functions/record_selection.py
@@ -14,8 +14,6 @@ def insertValuesToBox(tree, labels, boxes):
 
     values = selectRecord(tree)
 
-    old_values = []
-
     for n, label in enumerate(labels):
         label.grid(row=n, column=1, padx=10, pady=10)
 
@@ -23,7 +21,6 @@ def insertValuesToBox(tree, labels, boxes):
         box.grid(row=n, column=2, padx=10, pady=10)
         box.delete(0, END)
         box.insert(0, values[n])
-        old_values.append(values[n])
 
 
 # split to two functions, 1 to get values and 1 to put the values in the button. And run the function within bottom
@@ -31,18 +28,22 @@ def insertValuesToBox(tree, labels, boxes):
 
 def updateRecord(tree, boxes, columns: list):
 
-    values = selectRecord(tree)
+    values = list(selectRecord(tree))
 
-    updates = []
+    updates = {}
 
-    n = 0
+    # for n, item in enumerate(boxes):
+    #     v = item.get()
+    #     update = {columns[n]: v}
+    #     updates.append(update)
 
-    for item in boxes:
+
+    for n, item in enumerate(boxes):
         v = item.get()
-        update = {columns[n]: v}
-        updates.append(update)
-        n += 1
+        update = {values[n]: v}
+        updates[columns[n]] = update
 
-    print(f"Old values: {values}")
     print(f"New values: {updates}")
- 
+    
+
+    
